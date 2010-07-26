d145470aa61edb240202e1fb0e1fbcb3427147d8
lowellzhong <lowellzhong@deba6006-377b-7330-a727-7f5e30ed5a8f>
2010-07-26 16:27:23 +0000

    clean up repos

--- trunk/rpcserver.php
+++
@@ -1,8 +0,0 @@
-<?php
-$rpccfg['services'] = '/opt/app/rpcserver/services';
-$rpccfg['display_error'] = true;
-
-require("fastrpc/FastRPCDispatch.php");
-
-$server = new FastRPCDispatch();
-$server->run();
